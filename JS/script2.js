a8abc03d646de72adf8ef1f151b8219ebf68e7bb
chrisleg19 <110962234+chrisleg19@users.noreply.github.com>
2022-09-20 16:05:35 -0400

    homework update

---
+++ JS/script2.js
@@ -0,0 +1,21 @@
+
+let revenue = document.querySelector(".revenueNum")
+let cogs = document.querySelector(".cogsNum")
+let rawMat = document.querySelector(".rawMatNum")
+let freight = document.querySelector(".freightNum")
+let storage = document.querySelector(".storageNum")
+let grossProfit = document.querySelector(".grossProfitNum")
+let otherOpCosts = document.querySelector(".otherOpCostsNum")
+let indirectLabor = document.querySelector(".indirectLaborNum")
+let legal = document.querySelector(".legalNum")
+let officeEquip = document.querySelector(".officeEquipNum")
+let opProfit = document.querySelector(".opProfitNum")
+
+
+let start = ()=>{
+    revenue.textContent = 510000
+    cogs.textContent = 150000
+    rawMat.textContent = 30000
+    freight.te
+
+}
