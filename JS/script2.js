d643e194f11778f9f14084ca7b71a739481d5692
chrisleg19 <110962234+chrisleg19@users.noreply.github.com>
2022-09-20 16:29:07 -0400

    updated income stmnt

--- JS/script2.js
+++ JS/script2.js
@@ -3,6 +3,7 @@ let revenue = document.querySelector(".revenueNum")
 let cogs = document.querySelector(".cogsNum")
 let rawMat = document.querySelector(".rawMatNum")
 let freight = document.querySelector(".freightNum")
+let factoryLabor = document.querySelector(".factoryLaborNum")
 let storage = document.querySelector(".storageNum")
 let grossProfit = document.querySelector(".grossProfitNum")
 let otherOpCosts = document.querySelector(".otherOpCostsNum")
@@ -10,12 +11,26 @@ let indirectLabor = document.querySelector(".indirectLaborNum")
 let legal = document.querySelector(".legalNum")
 let officeEquip = document.querySelector(".officeEquipNum")
 let opProfit = document.querySelector(".opProfitNum")
+let opm = document.querySelector(".opmNum")
 
 
 let start = ()=>{
     revenue.textContent = 510000
     cogs.textContent = 150000
     rawMat.textContent = 30000
-    freight.te
+    freight.textContent = 20000
+    factoryLabor.textContent = 75000
+    storage.textContent = 25000
+    grossProfit.textContent = 360000
+    otherOpCosts.textContent = 300000
+    indirectLabor.textContent = 150000
+    legal.textContent = 50000
+    officeEquip.textContent = 100000
+    opProfit.textContent = 60000
 
+    opm.textContent = (opProfit.textContent/revenue.textContent)*100
 }
+
+start()
+
+
